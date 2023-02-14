c2c7e53f833537b7a1dd8dc277c7d0b07c27ad47
Richie-Peak <thomas.richardson@peak.ai>
2023-02-14 23:28:20 +0000

    made package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='Richie_utils',
+	version='0.1',
+	description='A utils package for me to practice package development',
+	url='https://github.com/Thomas-Richardson/Richie_utils',
+	author='Richie',
+	author_email='thomas.m.richardson@protonmail.com',
+	license='MIT',
+	packages=['Richie_utils'],
+    #install_requires=[],
+    zip_safe=False)
